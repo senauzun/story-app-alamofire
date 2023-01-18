0b512aa83a0aa218df0307c4c836b1e94095bfcc
senauzun <seenauzun@gmail.com>
2023-01-18 23:18:53 +0300

    Initial Commit

---
+++ StoryAppAlamofire/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StoryAppAlamofire
+//
+//  Created by Sena Uzun on 18.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoryAppAlamofire/StoryAppAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryAppAlamofireApp.swift
+//  StoryAppAlamofire
+//
+//  Created by Sena Uzun on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoryAppAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
